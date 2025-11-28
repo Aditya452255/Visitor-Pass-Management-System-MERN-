9d6c97c99c8c29e63c80c4fb3d70b377afaee440
Aditya45225 <adityajain0322@gmail.com>
2025-11-29 01:29:14 +0530

    chore: add frontend files into repository (remove nested git)

--- backend/scripts/debug_visitor_lookup.js
+++
@@ -1,4 +0,0 @@
-// Removed debug script: debug_visitor_lookup.js
-// Replaced with a harmless stub on 2025-11-29 to avoid affecting the site.
-console.log('This debug script has been removed.');
-process.exit(0);
--- backend/scripts/get_appointment_by_id.js
+++
@@ -1,4 +0,0 @@
-// Removed debug script: get_appointment_by_id.js
-// Replaced with a harmless stub on 2025-11-29 to avoid affecting the site.
-console.log('This debug script has been removed.');
-process.exit(0);
